10e63e5df593f4cde9cf9c6b9a32799f5fcfd1e2
chenshida <348004370@qq.com>
2019-02-03 14:16:30 +0800

    keil-mdk liteos transplant ok and compile pass

---
+++ MDK-ARM/STM32L476LiteOS/los_dispatch_keil.d
@@ -0,0 +1,1 @@
+stm32l476liteos\los_dispatch_keil.o: ..\MiddleWares\LiteOS\arch\arm\arm-m\cortex-m4\keil\los_dispatch_keil.S
--- Src/stm32l4xx_it.c
+++ Src/stm32l4xx_it.c
@@ -183,24 +183,24 @@ void DebugMon_Handler(void)
 //void PendSV_Handler(void)
 //{
 //  /* USER CODE BEGIN PendSV_IRQn 0 */
-//
+
 //  /* USER CODE END PendSV_IRQn 0 */
 //  /* USER CODE BEGIN PendSV_IRQn 1 */
-//
+
 //  /* USER CODE END PendSV_IRQn 1 */
 //}
 
-/**
-  * @brief This function handles System tick timer.
-  */
+///**
+//  * @brief This function handles System tick timer.
+//  */
 //void SysTick_Handler(void)
 //{
 //  /* USER CODE BEGIN SysTick_IRQn 0 */
-//
+
 //  /* USER CODE END SysTick_IRQn 0 */
 //  HAL_IncTick();
 //  /* USER CODE BEGIN SysTick_IRQn 1 */
-//
+
 //  /* USER CODE END SysTick_IRQn 1 */
 //}
 
